1d1a4bf9e51b3295591a1abde9ec62ec2934a82b
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-07-12 16:06:03 -0300

    Adicionando jQuery e fonte
    Usando a biblioteca jQuery, melhorando a responsividade e utilizando fonte nova

--- jogo_cores/jogo_cor.js
+++ jogo_cores/jogo_cor.js
@@ -1,60 +1,70 @@
-var cor_vermelho=125;
-var cor_verde=125;
-var cor_azul=125;
+var corVermelho = 125;
+var corVerde = 125;
+var corAzul = 125;
 
-var r=0;
-var g=0;
-var b=0;
+var r = 0;
+var g = 0;
+var b = 0;
 
-window.onload = function() {
+window.onload = function () {
+    configurarSliders();
     setarCorQuestao();
-  };
+};
 
-var sliderVermelho=document.querySelector("#slider_vermelho")
+const lataResultado = $(".lata_cor_resultado");
+const textoResultado = lataResultado.parent().find(".texto");
+function atualizarCorResultado() {
+    lataResultado.css("background-color", `rgb(${corVermelho}, ${corVerde}, ${corAzul})`);
+    textoResultado.find(".text-danger").text(corVermelho);
+    textoResultado.find(".text-success").text(corVerde);
+    textoResultado.find(".text-primary").text(corAzul);
 
-sliderVermelho.addEventListener('input', function() {
-    var vermelho=document.querySelector(".vermelho")
-    cor_vermelho=this.value;
-    vermelho.style.backgroundColor='rgb('+ this.value+ ',0,0)';
-    setCorResultado();
-  });
-
-  var sliderVerde=document.querySelector("#slider_verde")
-
-sliderVerde.addEventListener('input', function() {
-    var verde=document.querySelector(".verde")
-    cor_verde=this.value;
-    verde.style.backgroundColor='rgb(0,'+ this.value+ ',0)';
-    setCorResultado();
-  });
-
-var sliderAzul=document.querySelector("#slider_azul")
-
-sliderAzul.addEventListener('input', function() {
-    var azul=document.querySelector(".azul");
-    cor_azul=this.value;
-    azul.style.backgroundColor='rgb(0,0,'+ this.value+')';
-    setCorResultado();
-  });
-
-  function setCorResultado(){
-    var corResultado=document.querySelector(".lata_cor_resultado");
-    corResultado.style.backgroundColor='rgb('+cor_vermelho+','+cor_verde+','+cor_azul+')'
-    if(cor_vermelho==r && cor_verde==g && cor_azul==b ){
+    if (corVermelho == r && corVerde == g && corAzul == b) {
         newPopup();
     }
-  }
+}
 
-  function setarCorQuestao(){
-    r=Math.floor(Math.floor(Math.random() * 255)/5)*5;
-    g=Math.floor(Math.floor(Math.random() * 255)/5)*5;
-    b=Math.floor(Math.floor(Math.random() * 255)/5)*5;
-    var elementoCasaCorQuestao=document.querySelector(".lata_cor_questao");
-    elementoCasaCorQuestao.style.backgroundColor='rgb('+r+','+g+','+b+')'
-  }
+function setarCorQuestao() {
+    r = Math.floor(Math.floor(Math.random() * 255) / 5) * 5;
+    g = Math.floor(Math.floor(Math.random() * 255) / 5) * 5;
+    b = Math.floor(Math.floor(Math.random() * 255) / 5) * 5;
+    let objetivo = $(".lata_cor_questao");
+    objetivo.css("background-color", `rgb(${r}, ${g}, ${b})`);
+}
 
-  function newPopup(){
-    varWindow = window.open ('venceu.html', 'venceu')
-    }
+function newPopup() {
+    varWindow = window.open('venceu.html', 'venceu')
+}
 
+function configurarSliders() {
+    const sliderVermelho = $("#slider_vermelho");
+    const lataVermelha = $(".lata_cor.vermelho");
+    const textoVermelho = lataVermelha.parent().find(".valor");
+    sliderVermelho.on("input", function () {
+        corVermelho = $(this).val();
+        lataVermelha.css("background-color", `rgb(${corVermelho}, 0, 0)`);
+        console.log(textoVermelho);
+        textoVermelho.text(corVermelho);
+        atualizarCorResultado();
+    });
 
+    const sliderVerde = $("#slider_verde");
+    const lataVerde = $(".lata_cor.verde");
+    const textoVerde = lataVerde.parent().find(".valor");
+    sliderVerde.on("input", function () {
+        corVerde = $(this).val();
+        lataVerde.css("background-color", `rgb(0, ${corVerde}, 0)`);
+        textoVerde.text(corVerde);
+        atualizarCorResultado();
+    });
+
+    const sliderAzul = $("#slider_azul");
+    const lataAzul = $(".lata_cor.azul");
+    const textoAzul = lataAzul.parent().find(".valor");
+    sliderAzul.on("input", function () {
+        corAzul = $(this).val();
+        lataAzul.css("background-color", `rgb(0, 0, ${corAzul})`);
+        textoAzul.text(corAzul);
+        atualizarCorResultado();
+    });
+}
